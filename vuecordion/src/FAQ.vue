ab5783146a6c1cef3f3d5f91c08b25b880dd7893
jfdm <martins_julian@gmx.de>
2023-01-10 15:42:38 +0100

    just fiddling around

---
+++ vuecordion/src/FAQ.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="faq">
+    <div class="question">{{ faq.question }}</div>
+    <div class="answer">{{ faq.answer }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FAQ',
+    props: ['faq', 'index']
+}
+</script>
+
+<style>
+
+</style>
